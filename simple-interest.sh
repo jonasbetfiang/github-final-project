a8c036f6ab403b29f602636031cedf576bcb3896
jonasbetfiang <72328175+jonasbetfiang@users.noreply.github.com>
2022-10-05 04:46:24 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,8 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors: jonasbetfiang
-
+   # Additional Authors: 
+   # jonasbetfiang
    # Input:
    # p, principal amount
    # t, time period in years
